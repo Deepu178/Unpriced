5db0c4e4a99714edb80879c4cf02bbd463aaa7b5
Saurav Ganguly <sauravganguly2018@gmail.com>
2022-11-05 23:23:40 +0530

    used body-parser

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
+var bodyParser = require("body-parser");
+
+// var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const userController = require("../controller/userController");
 const router = express.Router();
 
-router.route("/").get(userController.getUsers).post(userController.createUser);
+router
+  .route("/")
+  .get(userController.getUsers)
+  .post(urlencodedParser, userController.createUser);
 //   .get(userController.getAllUsers)
 
 router
