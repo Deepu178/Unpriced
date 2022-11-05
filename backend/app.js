403c2d48fdce5faae47cf87f0b11c5f77e42cfe6
Saurav Ganguly <sauravganguly2018@gmail.com>
2022-11-05 21:41:25 +0530

    solved createUser error

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRoutes");
+app.use(express.json());
 app.use("/api/v1/users", userRouter);
 module.exports = app;
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,11 +2,23 @@ const User = require("../models/userModel");
 
 exports.createUser = async (req, res) => {
   const newUser = await User.create(req.body);
+  // console.log(req.body);
 
   res.status(201).json({
     status: "success",
     data: {
       user: newUser,
+    },
+  });
+};
+
+exports.getUsers = async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      users,
     },
   });
 };
